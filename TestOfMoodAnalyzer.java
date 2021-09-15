9539a213275eb31ffde0436da4dcd9b751f47f1d
NayanPunyarthi <punyarthinayan16@gmail.com>
2021-09-15 21:54:14 +0530

    UC2

--- TestOfMoodAnalyzer.java
+++ TestOfMoodAnalyzer.java
@@ -1,20 +1,30 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestOfMoodAnalyzer {
 
     @Test
     public void MoodTest_Sad(){
-        String moodMessage = "I am in Sad mood";
 
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        moodAnalyzer.analyzeMood(moodMessage);
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad mood");
+        String mood = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("SAD", mood);
     }
 
     @Test
     public void MoodTest_Happy(){
-        String moodMessage = "I am in Any mood";
 
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        moodAnalyzer.analyzeMood(moodMessage);
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Any mood");
+        String mood = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void MoodTest_Null(){
+
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String mood = moodAnalyzer.analyzeMood();
+        Assert.assertEquals("Happy", mood);
     }
 }
